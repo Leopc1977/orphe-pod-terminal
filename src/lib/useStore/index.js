46f574840e5ef382e76cae1e5898ae1fc9d0c1d9
Leopc1977 <46353973+Leopc1977@users.noreply.github.com>
2025-09-08 00:17:34 +0200

    omg added zustand owo

---
+++ src/lib/useStore/index.js
@@ -0,0 +1,11 @@
+import { create } from 'zustand'
+
+export default create((set) => ({
+    // game: null,
+    // initGame: (
+    //     playerNames: string[],
+    //     houseRules?: {
+    //         setup: (game) => void;
+    //     }[]
+    // ) => set(() => ({ game: new Game(playerNames, houseRules) })),
+}));
